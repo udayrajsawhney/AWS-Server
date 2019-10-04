ed983aa566f923361e02e7cf8c133f26cf3fe6bb
udayrajsawhney <30777357+udayrajsawhney@users.noreply.github.com>
2019-10-04 11:20:01 +0530

    updated structure of connection object

--- pubsub.js
+++ pubsub.js
@@ -1,9 +1,9 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 
-export default new RedisPubSub({
-  connection: {
-    host: '127.0.0.1',
-    port: 6379,
-    retry_strategy: options => Math.max(options.attempt * 100, 3000),
-  },
-});
+const testEndPoint = '127.0.0.1';
+const connectionObj = {
+  host: testEndPoint,
+  port: 6379,
+  retry_strategy: options => Math.max(options.attempt * 100, 3000),
+};
+export default new RedisPubSub({connectionObj});
